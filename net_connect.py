5a9ccfe0262e8bbb85d083903dff9db98dce68e1
ChasionNo1 <43226362+ChasionNo1@users.noreply.github.com>
2018-12-23 19:40:32 +0800

    Add files via upload

--- net_connect.py
+++ net_connect.py
@@ -13,3 +13,15 @@ def check_net():
     except requests.exceptions.ConnectionError:
         return '网络没有连接'
 
+
+def login(user, domain, psw):
+    url = 'http://a.nuist.edu.cn/index.php/index/login'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36'}
+    password = psw
+    data = {'username': user,
+            'domain': domain,
+            'password': base64.b64encode(password.encode()),
+            'enablemacauth': '0'}
+    response = requests.post(url, data=data, headers=headers)
+    return response
